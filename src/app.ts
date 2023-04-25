723d85edbe1bd995c41d807e73f0bc0d753e682b
bmartinez95 <bmartinezgarmendia@gmail.com>
2023-04-25 17:19:42 +0200

    node install and node_module added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import path from "path";
 
+
 import { loadApiEndpoints } from "./controllers/api";
 
 // Create Express server
 const app = express();
+const helmet = require('helmet');
 
 // Express configuration
 app.set("port", process.env.PORT ?? 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }));
 
